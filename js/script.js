aca03a2167988babd7d45d44a92f12f025199b56
Russell Nyorere <ecodenhub@gmail.com>
2018-10-25 12:50:07 +0100

    Added JS code to render date dynamically

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // Global Variable
 let slideIndex = 0;
 
-// IFFE Init Call to functions
+// IFFE Init Call to both functions
 (function init() {
     // Call image Carousel 
     imageCarousel();
+
+    // Call copy right year
+    copyRightYear();
 }());
 
 // Function to displays Image Carousel
@@ -18,4 +21,11 @@ function imageCarousel() {
     if (slideIndex > images.length) {slideIndex = 1} 
     images[slideIndex-1].style.display = "block"; 
     setTimeout(imageCarousel, 3000); // Change image every 3 seconds
+};
+
+// Function to get current year
+function copyRightYear() {
+    let currentYear = document.getElementById('year');
+    let year = new Date().getFullYear();
+    currentYear.innerHTML = year;
 };
