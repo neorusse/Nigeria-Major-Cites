25d566e6c7f8e88a8bcdc92e372c70a18b8ee730
Russell Nyorere <ecodenhub@gmail.com>
2018-10-25 20:44:49 +0100

    remove gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-
+const gulp = require('gulp'),
+    sass = require('gulp-sass')
+    autoprefixer = require('gulp-autoprefixer');
+    
 gulp.task('styles', function() {
         gulp.src("scss/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+        }))
         .pipe(gulp.dest('./css'));
-  });
+});
+
+gulp.task('watchSass', function() {
+	gulp.watch('scss/**/*.scss', ['styles']);
+});
   
