e157cf2527e456d122d1f55e9623b11d03648680
Russell Nyorere <ecodenhub@gmail.com>
2018-10-30 12:10:28 +0100

    Update to files due to addtion of prettier file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass')
-    autoprefixer = require('gulp-autoprefixer');
-    
-gulp.task('styles', function() {
-        gulp.src("scss/**/*.scss")
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-        }))
-        .pipe(gulp.dest('./css'));
+const gulp = require("gulp"),
+  sass = require("gulp-sass");
+autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("styles", function() {
+  gulp
+    .src("scss/**/*.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('watchSass', function() {
-	gulp.watch('scss/**/*.scss', ['styles']);
+gulp.task("watchSass", function() {
+  gulp.watch("scss/**/*.scss", ["styles"]);
 });
-  
--- js/script.js
+++ js/script.js
@@ -3,29 +3,31 @@ let slideIndex = 0;
 
 // IFFE Init Call to both functions
 (function init() {
-    // Call image Carousel 
-    imageCarousel();
+  // Call image Carousel
+  imageCarousel();
 
-    // Call copy right year
-    copyRightYear();
-}());
+  // Call copy right year
+  copyRightYear();
+})();
 
 // Function to displays Image Carousel
 function imageCarousel() {
-    let i;
-    let images = document.getElementsByClassName("banner__img-slides");
-    for (i = 0; i < images.length; i++) {
-      images[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > images.length) {slideIndex = 1} 
-    images[slideIndex-1].style.display = "block"; 
-    setTimeout(imageCarousel, 3000); // Change image every 3 seconds
-};
+  let i;
+  let images = document.getElementsByClassName("banner__img-slides");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > images.length) {
+    slideIndex = 1;
+  }
+  images[slideIndex - 1].style.display = "block";
+  setTimeout(imageCarousel, 3000); // Change image every 3 seconds
+}
 
 // Function to get current year
 function copyRightYear() {
-    let currentYear = document.getElementById('year');
-    let year = new Date().getFullYear();
-    currentYear.innerHTML = year;
-};
+  let currentYear = document.getElementById("year");
+  let year = new Date().getFullYear();
+  currentYear.innerHTML = year;
+}
